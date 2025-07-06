1b89ada433f65b8f77f9be3fdc1083f8ecdf797c
Jaska Pääkkönen <jaska.paakkonen@gmx.com>
2025-07-06 13:59:16 +0300

    loader updated

--- src/lib/Loading.svelte
+++ src/lib/Loading.svelte
@@ -13,14 +13,34 @@
     </div>
 </div-->
 <div class="wrap">
-    <div class="loader"></div>
+    <!-- svelte-ignore a11y_missing_attribute -->
+    <!--div class="loader"><img src="cards/suits2.svg" /></div-->
+    <!-- svelte-ignore a11y_missing_attribute -->
 </div>
+<!-- svelte-ignore a11y_missing_attribute -->
+<!--img class="loader" src="cards/suits2.svg" /-->
 
+<!-- svelte-ignore a11y_missing_attribute -->
+<img class="loader club" src="cards/club.svg" />
+<!-- svelte-ignore a11y_missing_attribute -->
+<img class="loader diamond" src="cards/diamond.svg" />
+<!-- svelte-ignore a11y_missing_attribute -->
+<img class="loader heart" src="cards/heart.svg" />
+<!-- svelte-ignore a11y_missing_attribute -->
+<img class="loader spade" src="cards/spade.svg" />
 <style>
     * {
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+    }
+
+    img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        margin-top: -25px;
+        margin-left: -25px;
     }
 
     .wrap {
@@ -33,19 +53,6 @@
         left: 40vw;
         z-index: 1000;
     }
-    .waviy {
-        position: relative;
-    }
-    .waviy span {
-        background-color: #00000099;
-        position: relative;
-        display: inline-block;
-        font-size: 4rem;
-        color: white;
-        text-transform: uppercase;
-        animation: flip 1s infinite;
-        animation-delay: calc(.2s * var(--i))
-    }
     @keyframes flip {
         0%,80% {
             transform: rotateY(360deg) 
@@ -53,17 +60,43 @@
     }
     .loader {
         position: absolute;
-        border: 16px solid #f3f3f3;
-        border-radius: 50%;
-        border-top: 16px solid #3498db;
-        width: 120px;
-        height: 120px;
-        -webkit-animation: spin 2s linear infinite; /* Safari */
-        animation: spin 1s linear infinite;
+        /*border: 5px solid white;
+        border-radius: 50%;*/
+        /*border-top: 25px solid #f3f3f3;*/
+        background-color: none;/*rgb(137, 137, 248);*/
+        width: 3vw;
+        height: 3vw;
+        /*-webkit-animation: spin2 2s linear infinite; /* Safari */
+        /*animation: spin2 3s linear infinite;*/
+        /*transform-origin:0%;*/
         margin: auto;
-        top: 30vh;
+        top: 50vh;
+        left: 48.5vw;
+        z-index: 2000;
+        transform-origin: 50% -5vw;
     }
-
+    .club {
+        /*transform-origin: 50% 0%;*/
+        -webkit-animation: spin4 1s linear infinite; /* Safari */
+        animation: spin4 1s linear infinite;
+    }
+    .diamond {
+        /*transform-origin: 100% 50%;*/
+        -webkit-animation: spin3 1s linear infinite; /* Safari */
+        animation: spin3 1s linear infinite;
+    }
+    .heart {
+        /*transform-origin: 0% 50%;*/
+        -webkit-animation: spin 1s linear infinite; /* Safari */
+        animation: spin 1s linear infinite;
+    }
+    .spade {
+        /*transform: translate(300px, 20px);*/
+        /*transform: rotate(0.25turn);
+        transform-origin: 50% 100%;*/
+        -webkit-animation: spin2 1s linear infinite; /* Safari */
+        animation: spin2 1s linear infinite;        
+    }
     /* Safari */
     @-webkit-keyframes spin {
         0% { -webkit-transform: rotate(0deg); }
@@ -73,5 +106,17 @@
     @keyframes spin {
         0% { transform: rotate(0deg); }
         100% { transform: rotate(360deg); }
+    }
+    @keyframes spin2 {
+        0% { transform: rotate(45deg); }
+        100% { transform: rotate(405deg); }
+    }
+    @keyframes spin3 {
+        0% { transform: rotate(90deg); }
+        100% { transform: rotate(450deg); }
+    }
+     @keyframes spin4 {
+        0% { transform: rotate(135deg); }
+        100% { transform: rotate(495deg); }
     }
 </style>
